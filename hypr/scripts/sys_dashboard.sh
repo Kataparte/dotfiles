1fc021a274cb5670bcb1bf0839c57d5abbd56047
Kataparte <101990463+Kataparte@users.noreply.github.com~>
2026-01-15 18:05:57 -0500

    feat(dash): replace cava with magenta cmatrix & polish fastfetch

--- hypr/scripts/sys_dashboard.sh
+++ hypr/scripts/sys_dashboard.sh
@@ -9,7 +9,11 @@ hyprctl dispatch exec [workspace 9] "kitty --class sys_btop -e btop"
 sleep 0.2
 
 # 3. Launch Fastfetch (Top Right)
-hyprctl dispatch exec [workspace 9] "kitty --class sys_fetch -e sh -c 'fastfetch; read'"
+#hyprctl dispatch exec [workspace 9] "kitty --class sys_fetch -e sh -c 'fastfetch; read'"
+#hyprctl dispatch exec [workspace 9] "kitty --class sys_fetch -o font_size=15 -o window_padding_width=30 -e sh -c 'fastfetch; read -n 1'"
+hyprctl dispatch exec [workspace 9] "kitty --class sys_fetch -o font_size=15 -o window_padding_width=60 -o placement_strategy=center -e sh -c 'fastfetch; read -n 1'"
 
 # 4. Launch Cava (Bottom Right)
-hyprctl dispatch exec [workspace 9] "kitty --class sys_visual -e cava"
+hyprctl dispatch exec [workspace 9] "kitty --class sys_matrix -e cmatrix -b -C magenta -u 7 "
+# Keep Cava running in the background for the Waybar
+hyprctl dispatch exec [workspace 9] "kitty --class sys_visual --minimize -e cava"
