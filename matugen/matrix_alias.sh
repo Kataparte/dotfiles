138dfdfa9c584ad909f3b2c8ca59d436a912a8d8
Kataparte <101990463+Kataparte@users.noreply.github.com~>
2026-01-16 21:27:34 -0500

    feat: automate sys_dashboard with matugen, switch to pipes.sh, and sync fastfetch/btop themes

---
+++ matugen/matrix_alias.sh
@@ -0,0 +1,3 @@
+# Using bash-pipes version
+alias matrix='pipes -c "#adc6ff"'
+alias pipes='pipes -c "#adc6ff"'
---
+++ matugen/templates/matrix_alias.sh
@@ -0,0 +1,3 @@
+# Using bash-pipes version
+alias matrix='pipes -c "{{colors.primary.default.hex}}"'
+alias pipes='pipes -c "{{colors.primary.default.hex}}"'
